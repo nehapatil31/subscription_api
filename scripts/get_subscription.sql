d64be21a4ddc4dbb94f81b05f09312ec8e10e38f
Neha <nehapatil317@gmail.com>
2021-04-26 02:51:01 +0530

    update scripts

--- scripts/get_subscription.sql
+++ scripts/get_subscription.sql
@@ -10,8 +10,13 @@ BEGIN
    select json_agg(json_build_object(
    	'plan_id', t.plan_id,
 	'start_date', t.start_date,   
-	 'end_date',(select t.start_date + interval '1' day * p.validity::int from plans as p 
-				 where p.plan_id=t.plan_id) 
+	'end_date',(
+		 CASE WHEN t.plan_id='FREE' THEN 'No end date'::VARCHAR
+		 ELSE(
+		 select (t.start_date + interval '1' day * p.validity::int)::VARCHAR from plans as p 
+				 where p.plan_id=t.plan_id
+		 )
+		END) 
    )) FROM subscriptions AS t WHERE user_name=username
    ));
 	return plans_json;
@@ -29,10 +34,15 @@ BEGIN
    plans_json:= json_build_object('data',(
    SELECT json_agg(json_build_object(
    	'plan_id', t.plan_id,  
-	'days_left',(SELECT t2.val - DATE_PART('day', date::timestamp - t.start_date::timestamp) FROM
+	'days_left',(
+		CASE WHEN t.plan_id='FREE' THEN 'Infinite'::VARCHAR
+		ELSE
+			(SELECT (t2.val - DATE_PART('day', date::timestamp - t.start_date::timestamp))::VARCHAR FROM
 				 (SELECT (p.validity::int)as val FROM plans AS p 
-				 WHERE p.plan_id=t.plan_id)t2) 
-   )) FROM subscriptions AS t WHERE user_name='neha' AND '2021-04-28'::date>t.start_date
+				 WHERE p.plan_id=t.plan_id)t2)
+		END) 
+	   
+   )) FROM subscriptions AS t WHERE user_name=username AND date::date>t.start_date
    ));
 	return plans_json;
 END;
